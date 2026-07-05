ffeae5e5039574868c2232bc2fe17869b63df9da
Peter Inzani <inzani.p@gmail.com>
2026-07-05 13:54:00 -0300

    updated prebuild_fdroid.sh to actually replace the ndk path

--- app/prebuild_fdroid.sh
+++ app/prebuild_fdroid.sh
@@ -10,7 +10,7 @@ cd ../Builder
 # Remove google service references from Builder build.gradle (probably not needed)
 sed -i -e '/enable = true/d' -e '/_appGdriveKey/d' -e '/_admob/d' -e '/googleServices/d' -e '/dep_/d' -e '/libDepFree/d' -e '/libDepPro/d' build.gradle
 # Replace author's ndk path with users
-sed -i -e 's|/Users/ivanivanenko/Library/Android/sdk/ndk|$USER_NDK_PATH|g' -e 's|/home/dev/Android/Sdk/ndk|$USER_NDK_PATH|g' link_to_mupdf_$MUPDF_VERSION.sh
+sed -i -e "s|/Users/ivanivanenko/Library/Android/sdk/ndk|$USER_NDK_PATH|g" -e "s|/home/dev/Android/Sdk/ndk|$USER_NDK_PATH|g" link_to_mupdf_$MUPDF_VERSION.sh
 #sed -i -e '1a set -e' -e '/git clone/d' -e 's/mkdir/mkdir -p/g' -e 's|"$PATH1/$NDK_VERSION/ndk-build"|"$$NDK$$/ndk-build"|' -e 's/$NDK /$NDK -j$(nproc) /' link_to_mupdf_$MUPDF_VERSION.sh
 #cp -r $$MuPDF$$ mupdf-$MUPDF_VERSION
 #rm -rf mupdf-$MUPDF_VERSION/thirdparty/{harfbuzz/test,curl/tests,leptonica/prog/fuzzing}
